ee0a988fe32d4549012051869d09e0c6bbce0c85
Philip Grabenhorst <phlip007@gmail.com>
2021-09-24 14:32:20 -0700

    Create class game, generates automata by numbered rules

---
+++ src/game.js
@@ -0,0 +1,79 @@
+
+/**
+ * @description 
+ */
+class Game {
+    ents = []
+
+    /**
+     * 
+     * @param {number} w 
+     * @param {number} h 
+     * @param {Array} start 
+     * @param {number} ruleset 8-bit number format, such that 0 <= rules <= 255
+     */
+    constructor(w, h, start, ruleset) {
+        // set instance variables
+        this.w = w
+        this.h = h
+        
+        // store ruleset as string
+        this.ruleset = (ruleset).toString(2).padStart(8, '0')
+        
+        // create the starting point
+        this.ents.push(start)
+
+        // generate the entities
+        this.generate()
+    }
+
+    generate() {
+        // creates a new row according to this.ruleset for the length of this.h
+        for (let i=0; i < this.h; i ++) {
+            this.generateNextRow()
+        }
+    }
+
+    generateNextRow() {
+        let last_r = this.ents[this.ents.length -1]
+        let new_r = new Array(this.w).fill(0)
+        for (let i = 0; i < this.w; i++) {
+            let parent_left
+            let parent_mid
+            let parent_right
+            if (i==0) { // handle left edge
+                parent_left = last_r[last_r.length - 1]
+                parent_mid = last_r[i]
+                parent_right = last_r[i + 1]
+            } else if (i== (this.w - 1)) { // handle right edge
+                parent_left = last_r[i - 1]
+                parent_mid = last_r[i]
+                parent_right = last_r[0]
+            } else { // everything in between
+                parent_left = last_r[i - 1]
+                parent_mid = last_r[i]
+                parent_right = last_r[i + 1]
+            }
+            // get rule to apply
+            let ri = parseInt(`${parent_left}${parent_mid}${parent_right}`, 2)
+            let new_ent = this.ruleset[7 - ri] // the ruleset is read right to left
+            // push ent to new row
+            new_r[i] = parseInt(new_ent)
+
+        }
+        // create new row
+        this.ents.push(new_r)
+    }
+
+    getRow(n) {
+
+    }
+
+    getWidth() {
+        return this.w
+    }
+
+    getHeight() {
+        return this.h
+    }
+}
